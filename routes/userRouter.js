9da9710ab88a53532eb2e2f3aa55f873822109c9
Tim Abrahamson <tim@kinetic.com>
2020-07-31 10:16:51 -0500

    Added login route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,7 @@
 
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 router.post("/register", async (req, res) => {
@@ -54,6 +55,25 @@ router.post("/login", async (req, res) => {
     // Validate
     if (!email || !password)
       return res.status(400).json({ msg: "Not all fields have been entered." });
+
+    const user = await User.findOne({ email: email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ msg: "No account with this email has been registered." });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        displayName: user.displayName,
+        email: user.email
+      }
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
